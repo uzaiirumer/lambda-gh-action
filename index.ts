74f65c0ec7e4114907a03a6034aae00447798cd7
root <root@localhost.localdomain>
2022-10-24 19:58:13 +0100

    init

---
+++ index.ts
@@ -0,0 +1,13 @@
+
+import { APIGatewayProxyEvent } from 'aws-lambda'; 
+import * as _ from 'lodash';
+export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
+
+const max -10;
+const val = _.random(max);
+ const response = {
+        statusCode: 200,
+        body: JSON.stringify'The random value (max ${max}) is: ${val}',
+    };
+    return response;
+};
