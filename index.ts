82e7964e2e45802845e74a45fea5a4c0a51ff488
root <root@localhost.localdomain>
2022-11-24 19:37:54 +0000

    New Max Number

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler} from "aws-lambd
 import * as _ from 'lodash';
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
 
-const max = 10;
+const max = 50;
 const val = _.random(max);
     const response = {
         statusCode: 200,
